fda03c79a92e0141ee397da8e9871dea9c4d058e
sofia fonsalido <sfonsalido@gmail.com>
2024-02-29 04:12:53 -0300

    colores

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,10 @@ export const App = () => {
       <NavBar />
       <ItemListContainer greeting={"Bienvenidos al E-Commerce de Galway Home"} />
       <ItemCount />
-      <Item product={{ title: "Dublin", description: "-Dinámica, Histórica-", marca: "Galway Home", stock: 30, price: 2675, img: "https://www.yomeanimo.com/wp-content/uploads/2021/11/los-imperdibles-de-irlanda-1-720x405-1.jpg" }} />
-      <Item product={{ title: "Corck", description: "-Vibrante, Acogedora-", marca: "Galway Home", stock: 40, price: 2365, img: "https://www.yomeanimo.com/wp-content/uploads/2021/11/los-imperdibles-de-irlanda-10-720x405-1.jpg" }} />
-      <Item product={{ title: "Galway", description: "-Cultural, Pintoresca-", marca: "Galway Home", stock: 40, price: 2500, img: "https://www.yomeanimo.com/wp-content/uploads/2021/11/los-imperdibles-de-irlanda-2-720x405-1.jpg" }} />
-      <Item product={{ title: "Dingle", description: "-Costero, Encantador-", marca: "Galway Home", stock: 40, price: 2330, img: "https://www.yomeanimo.com/wp-content/uploads/2021/11/los-imperdibles-de-irlanda-5-720x405-1.jpg" }} />
+      <Item product={{ title: "Dublin", description: "-Dinámica, Histórica-", marca: "Working Holiday", stock: 30, price: 2675, img: "https://www.yomeanimo.com/wp-content/uploads/2021/11/los-imperdibles-de-irlanda-1-720x405-1.jpg" }} />
+      <Item product={{ title: "Cork", description: "-Vibrante, Acogedora-", marca: "Working Holiday", stock: 40, price: 2365, img: "https://www.yomeanimo.com/wp-content/uploads/2021/11/los-imperdibles-de-irlanda-10-720x405-1.jpg" }} />
+      <Item product={{ title: "Galway", description: "-Cultural, Pintoresca-", marca: "Working Holiday", stock: 40, price: 2500, img: "https://www.yomeanimo.com/wp-content/uploads/2021/11/los-imperdibles-de-irlanda-2-720x405-1.jpg" }} />
+      <Item product={{ title: "Dingle", description: "-Costero, Encantador-", marca: "Working Holiday", stock: 40, price: 2330, img: "https://www.yomeanimo.com/wp-content/uploads/2021/11/los-imperdibles-de-irlanda-5-720x405-1.jpg" }} />
       <Dolar />
     </>
   )
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,12 +2,12 @@ export const Item = ({ product }) => {
     return (
         <div className=" max-w-xs mx-auto bg-gray-200 p-3 rounded-md shadow-md">
             <img src={product.img} alt={`Imagen de ${product.title}`} />
-            <h2 className="text-lg font-semibold font-bold mb-2">{product.title} {product.description}</h2>
-            <p className="text-gray-700 mb-2 font-semibold">{product.marca}</p>
+            <h2 className="text-lg font-semibold font-bold mb-2 text-center">{product.title} {product.description}</h2>
+            <p className="text-gray-700 mb-2 font-semibold font-style: italic">{product.marca}</p>
             <p className="text-gray-700 mb-2">Stock: {product.stock}</p>
             <div className="flex justify-between items-center">
                 <span className="text-xl font-bold">U$D{product.price}</span>
-                <button className="bg-cyan-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none">
+                <button className="bg-orange-600 text-white px-4 py-2 rounded-md hover:bg-green-700 focus:outline-none font-style: italic">
                     Add to Cart
                 </button>
             </div>
